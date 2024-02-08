31f71173cf854f6b64bfabe4264eb2213da724fa
Ксения Анисимова <anis.xenia@yandex.ru>
2024-02-08 16:42:35 +0300

    env a_h

--- backend/foodgram_project/foodgram_project/settings.py
+++ backend/foodgram_project/foodgram_project/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['158.160.72.87', '127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['158.160.72.87', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
